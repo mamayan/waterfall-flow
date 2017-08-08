9630a117f804d67a6922a9a0e22c4fa69e966210
马燕 <mosic.ma@hotmail.com>
2017-08-08 13:51:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 /**
  * Created by mayan on 2017/7/26.
  */
-var sass = require('gulp-sass');
-var gulp = require('gulp');
-gulp.task('default',function () {
-    gulp.src('index.scss')
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('sass',function () {
+   return gulp.src('index.scss')
         .pipe(sass())
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('dist/'))
     });
